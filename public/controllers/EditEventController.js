068aa250543f5ff709d1b7aa401a3bd3d8ee344d
Giorgia <giorgia1london@gmail.com>
2014-12-22 16:14:11 +0000

    validation with regEx and ng-pattern

--- public/controllers/EditEventController.js
+++ public/controllers/EditEventController.js
@@ -1,6 +1,7 @@
 eventsApp.controller('EditEventController',
  function EditEventController($scope){
  		$scope.saveEvent = function(event, newEventForm){
+ 				console.log(newEventForm);
  				if(newEventForm.$valid){
  			window.alert('event ' + event.name + ' you got it');
  		}
