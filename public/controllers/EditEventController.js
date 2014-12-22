e9a69f41f6efe9ea8b602d5d5c7fe0033d21f412
Giorgia <giorgia1london@gmail.com>
2014-12-22 13:48:51 +0000

    handling user input with 2 ways binding

--- public/controllers/EditEventController.js
+++ public/controllers/EditEventController.js
@@ -1,5 +1,11 @@
 eventsApp.controller('EditEventController',
  function EditEventController($scope){
+ 		$scope.saveEvent = function(event){
+ 			window.alert('event ' + event.name + ' you got it');
+ 		};
 
+ 		$scope.cancelEdit = function(){
+ 			window.location = "/newEvent";
+ 		}; 
 
 });
