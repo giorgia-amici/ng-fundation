8e9c5f32760256868944264b9c398042506c31f0
Giorgia <giorgia1london@gmail.com>
2014-12-23 22:36:38 +0000

    rescue me..lost in node

--- web-server.js
+++ web-server.js
@@ -5,13 +5,15 @@ var path = require('path')
 var fs = require('fs')
 var url = require('url')
 var events = require('events')
-// app.use(bodyParser.urlencoded());
+var bodyParser = require('body-parser')
+
 
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(request, response){
   response.render('index')
@@ -25,9 +27,13 @@ app.get('/editProfile', function(request, response){
   response.render('editProfile')
 });
 
-app.post('public/data/event', function(request, response){
-    var requestBody = req.body
-})
+app.post('/', function(request, response){
+    var event = request.body.event
+    console.log(event)
+  // response.render('index', {name: request.body.id});
+});
+
+
 
 server.listen(3000, function(){
   console.log("Server listening on port 3000");
