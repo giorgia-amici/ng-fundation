5c56e166b40a5af64cbdab31becd9ce379c2bcda
Giorgia <giorgia1london@gmail.com>
2014-12-23 16:31:39 +0000

    q library

--- public/services/EventData.js
+++ public/services/EventData.js
@@ -2,6 +2,7 @@ eventsApp.factory('eventData', function($http, $q) {
   return {
     getEvent: function() {
       var deferred = $q.defer();
+      // Creates a Deferred object which represents a task which will finish in the future.
 
         $http({method: 'GET', url: '/data/event/1.json'}).
           success(function (data, status, headers, config){ 
